c5932e114d7dc4f2f2404a9da63b0eb284ecce2b
Serge Buasa <sergebuasa@gmail.com>
2023-12-29 10:15:50 +0100

    premiers jets

---
+++ personnages.py
@@ -0,0 +1,29 @@
+class Hero:
+
+    def __init__(self, nom, nombre_point_de_vie, nombre_potions):
+        self.nom = nom
+        self.nombre_point_de_vie = nombre_point_de_vie
+        self.nombre_potions = nombre_potions
+
+    def choix_action(self):
+        pass
+
+    def attaquer_monstres(self):
+        pass
+
+    def utiliser_potion(self):
+        pass
+
+    def diademe_recupere(self):
+        pass
+
+
+class Monstre:
+
+    def __init__(self, nom, nombre_point_de_vie, niveau, nombre_de_degat_attaque):
+        pass
+
+    
+        
+        
+    
